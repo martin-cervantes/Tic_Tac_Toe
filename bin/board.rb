bd712bf5c6721c72d165e0595f9ddca254da20e3
edieatha <edieatha@gmail.com>
2020-05-19 18:32:19 +0300

    Added Instance variables to classes

--- bin/board.rb
+++ bin/board.rb
@@ -1,79 +1,44 @@
 class board
-
-def print_board 
- system "clear"
-
-  puts "+---------------------------+"
-
-  puts "|   T I C   T A C   T O E   |"
-
-  puts "+---------------------------+\n\n"
-
-  puts "Player 1 (X)  -  Player 2 (O)\n\n"
-
-  puts "Enter a number that corresponds to a point on the board\n\n"
-
-  puts " #{@square[0]} | #{@square[1]} | #{@square[2]} "
-
-  puts "-----------"
-
-  puts " #{@square[3]} | #{@square[4]} | #{@square[5]} "
-
-  puts "-----------"
-
-  puts " #{@square[6]} | #{@square[7]} | #{@square[8]} "
-
- end
-
-def checkwin 
-
-  if @square[0] == @square[1] && @square[1] == @square[2]
-
-    return 1
-
-  elsif @square[3] == @square[4] && @square[4] == @square[5]
-
-    return 1
-
-  elsif @square[6] == @square[7] && @square[7] == @square[8]
-
-    return 1
-
-  elsif @square[0] == @square[3] && @square[3] == @square[6]
-
-    return 1
-
-  elsif @square[1] == @square[4] && @square[4] == @square[7]
-
-    return 1
-
-  elsif @square[2] == @square[5] && @square[5] == @square[8]
-
-    return 1
-
-  elsif @square[0] == @square[4] && @square[4] == @square[8]
-
-    return 1
-
-  elsif @square[2] == @square[4] && @square[4] == @square[6]
-
-    return 1
-
-  elsif @square[0] != '1' && @square[1] != '2' && @square[2] != '3' &&
-
-        @square[3] != '4' && @square[4] != '5' && @square[5] != '6' &&
-
-        @square[6] != '7' && @square[7] != '8' && @square[8] != '9'
-
-   return 0
-
-  else
-
-   return  - 1
-
+  
+  def print_board 
+   system "clear"
+   puts "+---------------------------+"
+   puts "|   T I C   T A C   T O E   |"
+   puts "+---------------------------+\n\n"
+   puts "Player 1 (X)  -  Player 2 (O)\n\n"
+   puts "Enter a number that corresponds to a point on the board\n\n"
+   puts " #{@square[0]} | #{@square[1]} | #{@square[2]} "
+   puts "-----------"
+   puts " #{@square[3]} | #{@square[4]} | #{@square[5]} "
+   puts "-----------"
+   puts " #{@square[6]} | #{@square[7]} | #{@square[8]} "
   end
 
- end
+   def checkwin 
+     if @square[0] == @square[1] && @square[1] == @square[2]
+       return 1
+     elsif @square[3] == @square[4] && @square[4] == @square[5]
+       return 1
+     elsif @square[6] == @square[7] && @square[7] == @square[8]
+       return 1
+     elsif @square[0] == @square[3] && @square[3] == @square[6]
+       return 1
+     elsif @square[1] == @square[4] && @square[4] == @square[7]
+       return 1
+     elsif @square[2] == @square[5] && @square[5] == @square[8]
+       return 1
+     elsif @square[0] == @square[4] && @square[4] == @square[8]
+       return 1
+     elsif @square[2] == @square[4] && @square[4] == @square[6]
+       return 1
+     elsif @square[0] != '1' && @square[1] != '2' && @square[2] != '3' &&
+       @square[3] != '4' && @square[4] != '5' && @square[5] != '6' &&
+       @square[6] != '7' && @square[7] != '8' && @square[8] != '9'
+       return 0
+     else
+       return  - 1
+     end
+   end
 
    def get_choice (choice, mark)
      if choice == 1 && @square[0] == '1'
