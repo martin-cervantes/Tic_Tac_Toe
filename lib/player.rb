21cac77ee82def00ab2f0e8a5be835f96b3f4534
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-20 10:24:52 -0600

    Fix player class stickler errors

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,15 @@
-Class player
+#!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
+#
+# Tic Tac Toe Player
+#
+class Player
   attr_accessor :name, :mark
+
   def initialize(name, mark)
     @name = name
     @mark = mark
+  end
 end
-
