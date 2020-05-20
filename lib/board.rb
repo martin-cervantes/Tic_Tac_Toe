801349f6cdd4c7c44596a2cc2283bf89c9acef5b
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-20 10:46:31 -0600

    Fix board class stickler errors

--- lib/board.rb
+++ lib/board.rb
@@ -1,82 +1,103 @@
-class board
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+#
+# Tic Tac Toe Game
+#
+class Board
   def initialize
-    @square = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-  end
-  
-  def print_board 
-   system "clear"
-   puts "+---------------------------+"
-   puts "|   T I C   T A C   T O E   |"
-   puts "+---------------------------+\n\n"
-   puts "Player 1 (X)  -  Player 2 (O)\n\n"
-   puts "Enter a number that corresponds to a point on the board\n\n"
-   puts " #{@square[0]} | #{@square[1]} | #{@square[2]} "
-   puts "-----------"
-   puts " #{@square[3]} | #{@square[4]} | #{@square[5]} "
-   puts "-----------"
-   puts " #{@square[6]} | #{@square[7]} | #{@square[8]} "
+    @square = %w[0 1 2 3 4 5 6 7 8 9]
   end
 
-   def checkwin 
-     if @square[0] == @square[1] && @square[1] == @square[2]
-       return 1
-     elsif @square[3] == @square[4] && @square[4] == @square[5]
-       return 1
-     elsif @square[6] == @square[7] && @square[7] == @square[8]
-       return 1
-     elsif @square[0] == @square[3] && @square[3] == @square[6]
-       return 1
-     elsif @square[1] == @square[4] && @square[4] == @square[7]
-       return 1
-     elsif @square[2] == @square[5] && @square[5] == @square[8]
-       return 1
-     elsif @square[0] == @square[4] && @square[4] == @square[8]
-       return 1
-     elsif @square[2] == @square[4] && @square[4] == @square[6]
-       return 1
-     elsif @square[0] != '1' && @square[1] != '2' && @square[2] != '3' &&
-       @square[3] != '4' && @square[4] != '5' && @square[5] != '6' &&
-       @square[6] != '7' && @square[7] != '8' && @square[8] != '9'
-       return 0
-     else
-       return  - 1
-     end
-   end
+  def print_board
+    system 'clear'
+    puts '+---------------------------+'
+    puts '|   T I C   T A C   T O E   |'
+    puts "+---------------------------+\n\n"
+    puts "Player 1 (X)  -  Player 2 (O)\n\n"
+    puts "Enter a number that corresponds to a point on the board\n\n"
+    puts " #{@square[1]} | #{@square[2]} | #{@square[3]} "
+    puts '-----------'
+    puts " #{@square[4]} | #{@square[5]} | #{@square[6]} "
+    puts '-----------'
+    puts " #{@square[7]} | #{@square[8]} | #{@square[9]} "
+  end
 
-   def get_choice (choice, mark)
-     if choice == 1 && @square[0] == '1'
-       @square[0] = mark
-       return true
-     elsif choice == 2 && @square[1] == '2'
-       @square[1] = mark
-       return true
-     elsif choice == 3 && @square[2] == '3'
-       @square[2] = mark
-       return true
-     elsif choice == 4 && @square[3] == '4'
-       @square[3] = mark
-       return true
-     elsif choice == 5 && @square[4] == '5'
-       @square[4] = mark
-       return true
-     elsif choice == 6 && @square[5] == '6'
-       $square[5] = mark
-       return true
-     elsif choice == 7 && @square[6] == '7'
-       @square[6] = mark
-       return true
-     elsif choice == 8 && @square[7] == '8'
-       @square[7] = mark
-       return true
-     elsif choice == 9 && @square[8] == '9'
-       @square[8] = mark
-       return true
-     else    
-       puts "Invalid move. Press Enter to continue..."
-       return false
-     end
-   end
+  def check_horizontal1
+    if @square[1] == @square[2] && @square[2] == @square[3]
+      true
+    else
+      false
+    end
+  end
 
+  def check_horizontal2
+    if @square[4] == @square[5] && @square[5] == @square[6]
+      true
+    else
+      false
+    end
+  end
+
+  def check_horizontal3
+    if @square[7] == @square[8] && @square[8] == @square[9]
+      true
+    else
+      false
+    end
+  end
+
+  def check_vertical1
+    if @square[1] == @square[4] && @square[4] == @square[7]
+      true
+    else
+      false
+    end
+  end
+
+  def check_vertical2
+    if @square[2] == @square[5] && @square[5] == @square[8]
+      true
+    else
+      false
+    end
+  end
+
+  def check_vertical3
+    if @square[3] == @square[6] && @square[6] == @square[9]
+      true
+    else
+      false
+    end
+  end
+
+  def check_diagonal1
+    if @square[1] == @square[5] && @square[5] == @square[9]
+      true
+    else
+      false
+    end
+  end
+
+  def check_diagonal2
+    if @square[3] == @square[5] && @square[5] == @square[7]
+      true
+    else
+      false
+    end
+  end
+
+  def check_draw(num_choice)
+    num_choice == 9
+  end
+
+  def get_choice(choice, mark)
+    if @square[choice] != 'X' && @square[choice] != 'O' && choice >= 1 && choice <= 9
+      @square[choice] = mark
+      true
+    else
+      false
+    end
+  end
 end
-
-
